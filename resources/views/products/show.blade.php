4e2f337d10af8da62eb2206b05ac0e349e5473f5
ola-cpu <Hackerdetoday@gmail.com>
2020-12-21 13:56:09 +0100

    creaction d'une galeri d'image

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -4,21 +4,21 @@
 
 @section('content')
   <div class="col-md-12">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+      <div class="row no-gutters p-3 border rounded align-items-center flex-md-row mb-4 shadow-sm position-relative">
 
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-success">
+        <div class="col p-3 d-flex flex-column position-static">
+          <muted class="d-inline-block mb-2 text-success">
             
             @foreach ($product->categories as $category)
 
             {{ $category->name }}{{ $loop->last ? '' : ','}}
 
             @endforeach
-          </strong>
-          <h5 class="mb-0">{{ $product->title }}</h5>
-         <hr>
-          <p class="mb-auto text-muted">{!! $product->description !!}</p>
-          <strong class="mb-auto font-weight-normal text-success">{{ $product->getPrice() }}</strong>
+          </muted>
+          <h3 class="mb-4">{{ $product->title }}</h3>
+        
+          <span>{!! $product->description !!}</span>
+          <strong class="mb-4 display-4 text-success">{{ $product->getPrice() }}</strong>
 
           <form action="{{ route('cart.store') }}" method="POST"> 
 
@@ -26,17 +26,54 @@
              <input type="hidden" name="product_id" value="{{ $product->id }}">
              
 
-            <button type="submit" class="btn btn-success"><i class="fa fa-shopping-bag" aria-hidden="true"></i> Ajouter au panier</button>
+            <button type="submit" class="btn btn-success mb-2"><i class="fa fa-shopping-bag" aria-hidden="true"></i> Ajouter au panier</button>
 
           </form>
          
         </div>
         <div class="col-auto d-none d-lg-block">
-          <img src="{{ asset('storage/' . $product->image)  }}" alt="">
+          <img src="{{ asset('storage/' . $product->image)  }}" id="mainImage">
+           
+           <div class="mt-2">
+            @if ($product->images)
+            <img src="{{ asset('storage/' . $product->image)  }}" class="img-thumbnail" width="50" >
+
+              @foreach(json_decode($product->images,true) as $image)
+                  <img src="{{ asset('storage/' . $image)  }}" width="50" class="img-thumbnail">
+
+              @endforeach
+
+
+
+            @endif
+
+           </div>
         </div>
       </div>
     </div> 
   
 
 @endsection
+
+
+@section('extra-js')
+
+<script >
+  var mainImage = document.querySelector('#mainImage');
+
+  var thumbnails =  document.querySelectorAll('.img-thumbnail');
+
+
+  thumbnails.forEach((element) => element.addEventListener('click', changeImage));
+
+  function changeImage(e) {
+
+    mainImage.src =  this.src; 
+  }
+
+</script>
+
+
+@endsection
+
 
