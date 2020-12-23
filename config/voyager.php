2afcf5df0ef52de693741a7438b96f88ec45b088
ola-cpu <Hackerdetoday@gmail.com>
2020-12-23 15:08:10 +0100

    modifiction du vendor pour regle le pb de voyager

--- config/voyager.php
+++ config/voyager.php
@@ -133,7 +133,7 @@ return [
                 'icon_class' => 'voyager-person',
             ],
             'voyager::generic.home' => [
-                'route'        => '/admin',
+                'route'        => '/',
                 'icon_class'   => 'voyager-home',
                 'target_blank' => true,
             ],
